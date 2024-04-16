23e36df22118f82ac6e061dec6fe56b2552ec6a2
nicola1397 <nicola1397@outlook.it>
2024-04-16 21:24:24 +0200

    added number in api pagination

--- app/Http/Controllers/Api/ProjectApi.php
+++ app/Http/Controllers/Api/ProjectApi.php
@@ -10,7 +10,7 @@ class ProjectApi extends Controller
 {
     public function index()
     {
-        $projects = Project::select(['id', 'type_id', 'title', 'image_url'])->with(['technologies', 'type'])->paginate();
+        $projects = Project::select(['id', 'type_id', 'title', 'image_url', 'description'])->with(['technologies', 'type'])->paginate(10);
 
         foreach ($projects as $project) {
             $project->image = asset('/storage' . "/" . $project->image_url);
